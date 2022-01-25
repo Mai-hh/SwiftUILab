425c3fc8af431b06d1d6fed2ebb7d5ca1535e25d
Mai-hh <mohuaihaoxm@163.com>
2022-01-26 00:19:49 +0800

    msg

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Maihao on 2022/1/25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
