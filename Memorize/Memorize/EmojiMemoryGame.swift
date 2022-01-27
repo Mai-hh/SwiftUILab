8c66d0976e7aa9748027ee09575efebfa6d0777b
Mai-hh <mohuaihaoxm@163.com>
2022-01-28 01:14:26 +0800

    lecture 4

--- Memorize/Memorize/EmojiMemoryGame.swift
+++ Memorize/Memorize/EmojiMemoryGame.swift
@@ -7,18 +7,23 @@
 
 import SwiftUI
 
-class EmojiMemoryGame {
+class EmojiMemoryGame: ObservableObject {
     
-    static let emojis = ["😀", "😃", "😄", "😁","😆","😅","😂","🤣"]
+    static let emojis = ["😀", "😃", "😄", "😁", "😆", "😅", "😂", "🤣"]
     
     static func creatMemoryGame() -> MemoryGame<String> {
         MemoryGame<String>(numberOfPairsOfCards: 4) { pairIndex in EmojiMemoryGame.emojis[pairIndex]
         }
     }
     
-    private var model: MemoryGame<String> = EmojiMemoryGame.creatMemoryGame()
+    @Published private var model: MemoryGame<String> = EmojiMemoryGame.creatMemoryGame()
     
     var cards: Array<MemoryGame<String>.Card> {
         model.cards
+    }
+    
+    //MARK: - Intent(s)
+    func choose(_ card: MemoryGame<String>.Card) {
+        model.choose(card)
     }
 }
